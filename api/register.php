0fe1c690c8ed795d5d2eb193caea8f1c66620be1
purplenevermind-alt <purplenevermind@gmail.com>
2026-05-31 10:08:24 +0100

    Criar api/register.php

---
+++ api/register.php
@@ -0,0 +1,23 @@
+<?php
+require "../config/db.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$name = $data["name"];
+$email = $data["email"];
+$password = password_hash($data["password"], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $name, $email, $password);
+
+if ($stmt->execute()) {
+    echo json_encode([
+        "success" => true
+    ]);
+} else {
+    echo json_encode([
+        "success" => false,
+        "message" => "Erro ao registrar"
+    ]);
+}
